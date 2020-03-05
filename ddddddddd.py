f329e62facb11ffc41fcc041be30c63975f21ccb
SebastianKazimierski <61839332+SebastianKazimierski@users.noreply.github.com>
2020-03-05 17:21:56 +0100

    Add files via upload

---
+++ ddddddddd.py
@@ -0,0 +1,45 @@
+
+print("Janusz")
+print(2)
+
+'''
+komentarz
+blokowy
+'''
+
+#potegowanie
+
+potega = 2 ** 10
+print(potega)
+
+#pobieranie danych z klawiatury
+age1= 20
+print(type(age1))
+print("twój wiek: ",age1,"lata")
+
+###################
+
+surname="kowalski"
+firstChar =surname[0]
+print(firstChar)
+length-len(surname)
+print(lenght)
+
+lastChar=surbane[len(surname)-1]
+print(lastChar)
+
+lastChar=surname[-1]
+print(lastChar)
+
+#konwersja
+
+x="5"
+print(type(x))
+x-int(x)
+print(type(x))
+
+y=4
+print(type(y)) #int
+y=y/2
+print(type(y))
+print(y) #2.0
